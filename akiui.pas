e91ff2bab3a8437c1597e76264c879a80f92dd30
and3md <and3md@gmail.com>
2022-04-02 18:46:14 +0200

    Simple TAButton widget and adding/removing widget methods.

--- akiui.pas
+++ akiui.pas
@@ -48,7 +48,6 @@ type
     function GetCallbackByIndex(Index: Integer): TSignalCallback;
     procedure AddCallback(SignalCallback: TSignalCallback);
 
-    constructor Create;
     destructor Destroy; override;
   end;
 
@@ -100,18 +99,53 @@ type
       ADefaultHeight: Integer);
     destructor Destroy; override;
 
-    procedure Add(Widget: TAWidget);
+    procedure AddWidget(Widget: TAWidget);
+    procedure RemoveWidget(Widget: TAWidget);
 
     property Application: TAApplication read FApplication;
+    property OnDestroy: TSignal read FOnDestroySignal;
   end;
 
+  { Base class for all widgets }
   TAWidget = class (TAObject)
+  strict private
+    { Can be set only on InitBackend in this class }
+    FInitializedBackend: Boolean;
+    FDestroySignalID: gulong;
+  private
+    FOnDestroySignal: TSignal;
+    procedure DestroySignal;
   protected
     FGtkWidget: PGtkWidget;
     FParent: TAObject;
 
     procedure InitBackend; virtual;
   public
+    constructor Create;
+    destructor Destroy; override;
+
+
+    property InitializedBackend: Boolean read FInitializedBackend;
+    property Parent: TAObject read FParent;
+
+    property OnDestroy: TSignal read FOnDestroySignal;
+  end;
+
+  { Simple button class }
+  TAButton = class (TAWidget)
+  strict private
+    FTitle: String;
+
+  private
+    FOnClickedSignal: TSignal;
+    procedure ClickedSignal;
+  protected
+    procedure InitBackend; override;
+  public
+    constructor Create(const ATitle: String);
+    destructor Destroy; override;
+
+    property OnClicked: TSignal read FOnClickedSignal;
   end;
 
 var
@@ -120,11 +154,105 @@ var
 
 implementation
 
+{ TAButton }
+
+procedure clicked_TAButton(Button: PGtkButton; UserData: GPointer); cdecl;
+var
+  AButton: TAButton;
+begin
+  AButton := TAButton(UserData);
+  AButton.ClickedSignal;
+end;
+
+procedure TAButton.ClickedSignal;
+var
+  I: Integer;
+begin
+  for I := 0 to FOnClickedSignal.Count -1 do
+  begin
+    FOnClickedSignal.GetCallbackByIndex(i)(Self);
+  end;
+end;
+
+procedure TAButton.InitBackend;
+begin
+  FGtkWidget := gtk_button_new_with_label(Pgchar(FTitle));
+  g_signal_connect_data(FGtkWidget, 'clicked', TGCallback(@clicked_TAButton), Self, nil, 0);
+
+  { Should be always last in InitBackend }
+  inherited InitBackend;
+end;
+
+constructor TAButton.Create(const ATitle: String);
+begin
+  FTitle := ATitle;
+  FOnClickedSignal := TSignal.Create;
+end;
+
+destructor TAButton.Destroy;
+begin
+  FreeAndNil(FOnClickedSignal);
+  inherited Destroy;
+end;
+
 { TAWidget }
+
+procedure TAWidget.DestroySignal;
+var
+  I: Integer;
+begin
+  for I := 0 to FOnDestroySignal.Count -1 do
+  begin
+    FOnDestroySignal.GetCallbackByIndex(i)(Self);
+  end;
+end;
+
+procedure destroy_TAWidget(GtkWidget: PGtkWidget; UserData: GPointer); cdecl;
+var
+  Widget: TAWidget;
+  Window: TAWindow;
+begin
+  Widget := TAWidget(UserData);
+  Widget.DestroySignal;
+
+  if Widget.Parent is TAWindow then
+  begin
+    Window := TAWindow(Widget.Parent);
+    Window.RemoveWidget(Widget);
+  end;
+
+  Window.FGtkWindow := nil; // do not try destroy it in destructor
+  FreeAndNil(Widget);
+  Writeln('Widget destroyed.');
+end;
 
 procedure TAWidget.InitBackend;
 begin
+  if FInitializedBackend then
+    Exit;
 
+  if FGtkWidget <> nil then
+  begin
+    FDestroySignalID := g_signal_connect_data(FGtkWidget, 'destroy', TGCallback(@destroy_TAWidget), Self, nil, 0);
+    FInitializedBackend := true;
+  end else
+    Writeln('Widget not initialized correctly. Maybe missed inherited that should be on end.');
+end;
+
+constructor TAWidget.Create;
+begin
+  FOnDestroySignal := TSignal.Create;
+end;
+
+destructor TAWidget.Destroy;
+begin
+  if (FGTKWidget <> nil) then
+  begin
+    g_signal_handler_disconnect(FGTKWidget, FDestroySignalID);
+    gtk_widget_destroy(FGTKWidget);
+    Writeln('Widget destroyed 2.');
+  end;
+  inherited Destroy;
 end;
 
 { TSignal }
@@ -146,11 +274,6 @@ begin
   if FEventCallbackList = nil then
     FEventCallbackList := TProcedureOfObjectList.Create;
   FEventCallbackList.Add(TProcedureOfObject(SignalCallback));
-end;
-
-constructor TSignal.Create;
-begin
-
 end;
 
 destructor TSignal.Destroy;
@@ -215,7 +338,7 @@ end;
 
 { TAWindow ------------------------------------------------------------------- }
 
-{ Funkcja obsługi sygnału aktywacji aplikacji. }
+{ When GTK Window is destroyed destroy our TAWindow. }
 procedure destroy_TAWindow(GtkApp: PGtkWidget; UserData: GPointer); cdecl;
 var
   Window: TAWindow;
@@ -228,7 +351,7 @@ begin
     Window.Application.MainWindow := nil;
 
   FreeAndNil(Window);
-  Writeln('zniszczno');
+  Writeln('Window destroyed.');
 end;
 
 procedure TAWindow.InitBackend;
@@ -243,10 +366,10 @@ begin
     g_signal_connect_data(FGtkWindow, 'destroy', TGCallback(@destroy_TAWindow), Self, nil, 0);
 
     { add children widgets }
-
     for I := 0 to FWidgetList.Count -1 do
     begin
       FWidgetList[I].InitBackend;
+      gtk_container_add(PGtkContainer(FGtkWindow), FWidgetList[I].FGtkWidget);
     end;
 
     FState := wsInitializedBackend;
@@ -257,7 +380,6 @@ procedure TAWindow.DestroySignal;
 var
   I: Integer;
 begin
-  { Other functions from signals }
   for I := 0 to FOnDestroySignal.Count -1 do
   begin
     FOnDestroySignal.GetCallbackByIndex(i)(Self);
@@ -288,12 +410,25 @@ begin
   inherited Destroy;
 end;
 
-procedure TAWindow.Add(Widget: TAWidget);
+procedure TAWindow.AddWidget(Widget: TAWidget);
 begin
   FWidgetList.Add(Widget);
   Widget.FParent := Self;
   if FState = wsInitializedBackend then
+  begin
     Widget.InitBackend;
+    gtk_container_add(PGtkContainer(FGtkWindow), Widget.FGtkWidget);
+  end;
+
+
+end;
+
+procedure TAWindow.RemoveWidget(Widget: TAWidget);
+begin
+  Assert(Widget.FParent = Self, 'Trying remove widget with other Parent.');
+
+  FWidgetList.Remove(Widget);
+  Widget.FParent := nil;
 end;
 
 
